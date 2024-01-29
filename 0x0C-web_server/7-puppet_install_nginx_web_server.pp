2ac8590159056448c412ca4d37188122d5e88a31
ameralnas <amer.live477@gmail.com>
2024-01-29 22:59:01 +0200

    Hi now

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+package { 'Nginx'
+    ensure => 'present'
+}
+
+exec { 'installing'
+    command => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+    provider => shell
+}
+
+exec { 'file'
+    commmand => 'echo "Helo World!" | sudo tee /var/www/html/index.html',
+    provider => shell
+}
+exec { 'redirecting'
+    command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:www.facebook.com permanent;/" /etc/nginx/sites-available/default'
+    provider => shell
+}
+exec { 'finishing'
+    command => 'sudo service nginx restart'
+    provider => shell
+}
