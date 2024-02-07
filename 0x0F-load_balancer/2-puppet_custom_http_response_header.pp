acd2999cc893fd19f9c7049f0eb3778e23b00b29
ameralnas <amer.live477@gmail.com>
2024-02-07 04:14:12 +0200

    hi

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,31 @@
 ##I just want to finish this task
-package { 'nginx':
-    ensure => 'present'
+exec { 'nginx':
+    command  => 'sudo apt-get -y install nginx',
+    provider => shell,
+    before   => Exec['file']
 }
 
 exec { 'installing':
-    command => 'sudo apt-get -y update; sudo apt-get -y install nginx',
-    provider => shell
+    command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+    provider => shell,
+    before   => Exec['nginx']
 }
 
 exec { 'file':
-    command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    provider => shell,
+    before   => Exec['theCommand']
 }
 
 exec { 'theCommand':
-    command => sudo sed -i "s#listen 80 default_server;#listen 80 default_server;\n\\tadd_header X-Served-By $HOSTNAME;#" /etc/nginx/sites-enabled/default
-    provider => shell
 
+    command     => 'sudo sed -i "s#listen 80 default_server;#listen 80 default_server;\n\\tadd_header X-Served-By $HOSTNAME;#" /etc/nginx/sites-enabled/default'
+    provider    => shell,
+    environment => ["HOSTNAME=${hostname}"],
+    before      => Exec['finishing']
 }
 
 exec { 'finishing':
-    command => 'sudo service nginx restart',
+    command  => 'sudo service nginx restart',
     provider => shell
 }
