33ea97c72b01a45c602d365bc7f0d1e85e3cff9e
ameralnas <amer.live477@gmail.com>
2024-02-07 04:29:19 +0200

    Go now plz

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,8 @@
-##I just want to finish this task
-exec { 'nginx':
-    command  => 'sudo apt-get -y install nginx',
-    provider => shell,
-    before   => Exec['file']
-}
-
-exec { 'installing':
-    command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
-    provider => shell,
-    before   => Exec['nginx']
-}
-
-exec { 'file':
-    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell,
-    before   => Exec['theCommand']
-}
-
-exec { 'theCommand':
-
-    command     => 'sudo sed -i "s#listen 80 default_server;#listen 80 default_server;\n\\tadd_header X-Served-By $HOSTNAME;#" /etc/nginx/sites-enabled/default'
-    provider    => shell,
-    environment => ["HOSTNAME=${hostname}"],
-    before      => Exec['finishing']
-}
-
-exec { 'finishing':
-    command  => 'sudo service nginx restart',
-    provider => shell
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
