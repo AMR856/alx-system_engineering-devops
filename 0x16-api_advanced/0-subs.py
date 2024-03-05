efd690ae9badacb6089c980464025ea6d2b3b5a1
ameralnas <amer.live477@gmail.com>
2024-03-05 20:46:36 +0200

    Add this

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,9 @@
 import requests
 import json
 
+
 def number_of_subscribers(subreddit):
+    """A function to do stuff that can be done using other functions"""
     if subreddit is None or str(subreddit) == "":
         return 0
     user_agent = "0x16-api_advanced/1.0 by AMR856"
