710359d9ebfb24e2e9af28bc92533f4b02f91ec7
ameralnas <amer.live477@gmail.com>
2024-03-05 21:36:34 +0200

    This one goes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """A function to do stuff that can be done using other functions"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    user_agent = "0x16-api_advanced/1.0 by AMR856"
-    headers = {"User-Agent": user_agent}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    my_json_object = requests.get(url, headers=headers, allow_redirects=False)
-    if my_json_object.status_code == 200:
-        my_dict = json.loads(my_json_object.text)
-        return my_dict['data']['subscribers']
-    else:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
