374f4d7f9e30d9ac11f3b48e85256514cb536292
ameralnas <amer.live477@gmail.com>
2024-03-06 22:10:02 +0200

    Try this

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """First script that uses Reddit API"""
-import json
 import requests
 
 
@@ -11,9 +10,9 @@ def number_of_subscribers(subreddit):
     user_agent = "0x16-api_advanced/1.0 by AMR856"
     headers = {"User-Agent": user_agent}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    my_json_object = requests.get(url, headers=headers, allow_redirects=False)
-    if my_json_object.status_code == 200:
-        my_dict = json.loads(my_json_object.text)
+    my_object = requests.get(url, headers=headers, allow_redirects=False)
+    my_dict = my_object.json()
+    if my_object.status_code == 200:
         return my_dict['data']['subscribers']
     else:
         return 0
